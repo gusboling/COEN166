c118c21e8061d9aadd5a4b6792cb75e77f33ee80
ShadyProphet <geekish2428@gmail.com>
2016-06-04 19:24:07 -0700

    Added vegetation sensing for percept-rows.

--- Agents/MyAgent/main.scm
+++ Agents/MyAgent/main.scm
@@ -28,11 +28,17 @@
 ;------------------------------
 ;  TARGET-BASED A* NAVIGATION
 ;------------------------------
-;This function takes the current percept as an argument, and returns a frontier.
-;The frontier is a list of tuples, with each tuple representing a vegetation square's
-;coordinates relative to the agent. The frontier list is sorted according to the hueristic
-;value of each vegetation square, plus the cost of reaching it.
-(define (get-frontier percept frontier previous_events)) #f)
+;This function returns a list of different target-squares (just vegetation squares for now, but possibly expanded later).
+;The list is sorted from most to least desirable, meaning that (car (get-frontier ... )) returns the details of the best
+;target. Desirability is a function of A) square type, B) proximity to predators, C) distance to square, and D) proximity
+;to other agents - other factors may be included later, but for now this is it.
+;
+;Here's my thoughts on this: the percept isn't constant, so I can't use it as a constant over multiple turns. That said,
+;the plants won't move (much?) so if I record their position relative to myself, and set that information as a variable,
+;then I can track them over multiple turns. This makes a couple of assumptions: first that an update algorithm will be
+;relatively easy to write, and second, that a simple navigation algorithm will be able to turn target data into a
+;good move (assuming the target is good, so three assumptions I guess).
+(define (get-frontier percept previous_events frontier) ("NOTHING YET"))
 
 (define (get-row-targets row y)
 	(if (null? row)
@@ -48,7 +54,7 @@
         (#t (row-targets (cdr row) y))
 			)
 		)
-	)
+  )
 )
 
 
